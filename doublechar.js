5b9feec62270f5b8adc80aef9f59b8ece3d8326e
Florian <36693992+steelersbcn@users.noreply.github.com>
2018-10-24 20:27:27 +0200

    Create doublechar.js

---
+++ doublechar.js
@@ -0,0 +1,22 @@
+/*
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+doubleChar("1234!_ ") ==> "11223344!!__  "
+Good Luck!
+*/
+
+function doubleChar(str) {
+  // Your code here
+  let newStr = "";
+  for(let i = 0; i < str.length; i++) {
+    newStr += str[i]+str[i];
+  }
+  return newStr;
+}
+
+console.log(doubleChar("abcd"), "aabbccdd"));
+console.log(doubleChar("Adidas"), "AAddiiddaass"));
